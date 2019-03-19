6b726f93b5ef5294ae41beafa33ebfde222fff50
FuzzyPumpkin <43913621+FuzzyPumpkin@users.noreply.github.com>
2019-03-18 22:36:29 -0500

    Update Kitsunegotchi.js

--- TimeWasters/Kitsunegotchi/Kitsunegotchi.js
+++ TimeWasters/Kitsunegotchi/Kitsunegotchi.js
@@ -1,4 +1,3 @@
-
 //motive tracking
 var hungerLvl = 3;
 var amusementLvl = 7;
@@ -7,29 +6,33 @@ var energyLvl = 2;
 //motive button click functions
 $("#energyBtn").click(function(){
 	if(energyLvl < 10){
-		var newLvl = $(".energy")[energyLvl]
+		var newLvl = $(".energy")[energyLvl];
 		newLvl.classList.add("fulfilled");
-		energyLvl++
+		energyLvl++;
+		document.getElementById("kitsuneImg").setAttribute("src", "images/sleep.png");
 	};
 });
 $("#amusementBtn").click(function(){
 	if(amusementLvl < 10){
-		var newLvl = $(".amuse")[amusementLvl]
+		var newLvl = $(".amuse")[amusementLvl];
 		newLvl.classList.add("fulfilled");
-		amusementLvl++
+		amusementLvl++;
+		document.getElementById("kitsuneImg").setAttribute("src", "images/play.png");
 	};
 });
 $("#hungerBtn").click(function(){
 	if(hungerLvl < 10){
-		var newLvl = $(".hunger")[hungerLvl]
+		var newLvl = $(".hunger")[hungerLvl];
 		newLvl.classList.add("fulfilled");
-		hungerLvl++
+		hungerLvl++;
+		document.getElementById("kitsuneImg").setAttribute("src", "images/eating.png");
 	};
 });
 $("#groomingBtn").click(function(){
 	if(groomingLvl < 10){
-		var newLvl = $(".groom")[groomingLvl]
+		var newLvl = $(".groom")[groomingLvl];
 		newLvl.classList.add("fulfilled");
-		groomingLvl++
+		groomingLvl++;
+		document.getElementById("kitsuneImg").setAttribute("src", "images/brush.png");
 	};
 });
