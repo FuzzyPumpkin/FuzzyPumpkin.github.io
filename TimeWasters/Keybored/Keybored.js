be307fd58af10214538b38026b564a8f1dd86ecd
Kari Minger <43913621+FuzzyPumpkin@users.noreply.github.com>
2019-03-23 11:37:13 -0500

    Library for special sounds
    Only set sounds for 9 keys, rest are default. Done for practice, not polish.

--- TimeWasters/Keybored/Keybored.js
+++ TimeWasters/Keybored/Keybored.js
@@ -1,1 +1,56 @@
-//temp file
+	var keyData = {
+		a: {
+			color: "purple",
+			sound: new Howl({
+			src: ['resources/sounds/bubbles.mp3']
+			})
+		},
+		s: {
+			color: "red",
+			sound: new Howl({
+			src: ['resources/sounds/confetti.mp3']
+			})
+		},
+		d: {
+			color: "white",
+			sound: new Howl({
+			src: ['resources/sounds/moon.mp3']
+			})
+		},
+		f: {
+			color: "green",
+			sound: new Howl({
+			src: ['resources/sounds/glimmer.mp3']
+			})
+		},
+		g: {
+			color: "blue",
+			sound: new Howl({
+			src: ['resources/sounds/glimmer.mp3']
+			})
+		},
+		h: {
+			color: "orange",
+			sound: new Howl({
+			src: ['resources/sounds/moon.mp3']
+			})
+		},
+		j: {
+			color: "pink",
+			sound: new Howl({
+			src: ['resources/sounds/pinwheel.mp3']
+			})
+		},
+		k: {
+			color: "aqua",
+			sound: new Howl({
+			src: ['resources/sounds/strike.mp3']
+			})
+		},
+		l: {
+			color: "indigo",
+			sound: new Howl({
+			src: ['resources/sounds/squiggle.mp3']
+			})
+		}
+	};
