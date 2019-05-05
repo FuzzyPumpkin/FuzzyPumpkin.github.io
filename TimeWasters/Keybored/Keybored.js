8a6c6e98334c373f4fdd143b41bbbb117e292edf
Kari <kariminger@gmail.com>
2019-05-05 17:43:21 -0500

    Random colors/cleanup
    Split paper.js script to separate file and removed keyData object as not needed once random colors were used and Howler.js eliminated.
    
    Still need to add click event listeners. Wish I could merge sound and visual js files, but paper.js craps out about it.

--- TimeWasters/Keybored/Keybored.js
+++ TimeWasters/Keybored/Keybored.js
@@ -1,14 +1,34 @@
-	var keyData = {
-		a: {color: "purple"},
-		s: {color: "red"},
-		d: {color: "black"},
-		f: {color: "green"},
-		g: {color: "blue"},
-		h: {color: "orange"},
-		j: {color: "pink"},
-		w: {color: "aqua"},
-		e: {color: "indigo"},
-		t: {color: "purple"},
-		y: {color: "red"},
-		u: {color: "black"}
-	};
+var message = "hello";
+	var circles = [];
+	function onKeyDown(event){
+		makeCircle();
+		};
+		
+    function makeCircle() {
+        var maxPoint = new Point(view.size.width, view.size.height);
+				var randomPoint = Point.random();
+				var point = maxPoint * randomPoint;
+        var newCircle = new Path.Circle(point, 100);
+        var keyData = ["a","s","d","f","g","h","j","w","e","t","y","u"];
+        var colors = ["black", "purple", "blue", "red", "green"];
+				if(keyData.includes(event.key)){
+					var randNum = Math.floor(Math.random() * 5);
+					var pickedColor = colors[randNum];
+					newCircle.fillColor = pickedColor;
+				}
+				else {
+					newCircle.fillColor = "transparent";
+				};
+		circles.push(newCircle);
+    };
+    
+	function onFrame(event){
+		for(var i = 0; i < circles.length; i++){
+			circles[i].fillColor.hue += .5;
+			circles[i].scale(.97);
+			if(circles[i].ares <1){
+				circles[i].remove();
+				circles.splice(i, 1);
+			};
+		};
+  };
