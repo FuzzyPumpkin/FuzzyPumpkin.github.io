d95f796d39bcf5d0e9bdc62b803f8c95e900d5b6
Kari <kariminger@gmail.com>
2019-05-17 22:15:14 -0500

    Started coding the JS
    and ran into something I don't know how to do. Yay!

---
+++ ReimburseMe/flex.js
@@ -0,0 +1,26 @@
+const addButton = document.querySelector(".addButton");
+const list = document.querySelector("ul");
+//need a change handler, I think. As is, it blanks out the lines as it's not storing the values.
+function createLine (){
+    listText = list.innerHTML;
+    newItemHTML = `
+        <li>
+			<input type="date" class="date" name="flexDate"></input>
+			<select class="type" name="flexType">
+				<option>Medical</option>
+				<option>Parking</option>
+				<option>Dependent</option>
+			</select>
+			<input type="text" placeholder="Description" class="desc" name="flexDesc"></input>
+			$<input type="number" class="amt" name="flexAmt" value="0"></input>
+			<button class="addButton">+</button>
+			</li>
+    `;
+    list.innerHTML = listText + newItemHTML;
+    let allAddButtons = document.querySelectorAll(".addButton");
+    for(let i = 0; i < allAddButtons.length; i++){
+		allAddButtons[i].addEventListener("click", createLine);
+	};
+}
+
+addButton.addEventListener("click", createLine);
