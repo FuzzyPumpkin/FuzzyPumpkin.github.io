007ed4d90b7a1b36aee39e4f121d813be6f3957c
Kari <kariminger@gmail.com>
2019-05-21 19:55:07 -0500

    Finalized index layout
    Still need to find a stock photo for realtor headshot and replace lorem ipsum.

--- RealEstate/realEstate.js
+++ RealEstate/realEstate.js
@@ -1,9 +1,7 @@
-//add a timing feature to call showSlides every 20s or so.
-
-var slideIndex = 1;
+let slideIndex = 1;
 showSlides(slideIndex);
 
-function plusSlides(n) {
+function moveSlides(n) {
   showSlides(slideIndex += n);
 }
 
@@ -12,17 +10,11 @@ function currentSlide(n) {
 }
 
 function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
+  const slides = document.getElementsByClassName("mySlides");
   if (n > slides.length) {slideIndex = 1} 
     if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
+    for (let i = 0; i < slides.length; i++) {
       slides[i].style.display = "none"; 
     }
-    for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-    }
   slides[slideIndex-1].style.display = "block"; 
-  dots[slideIndex-1].className += " active";
 }
