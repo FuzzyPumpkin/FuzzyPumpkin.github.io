af7b3c8658cfd386f32282741c1f38bd93730b46
Kari <kariminger@gmail.com>
2019-06-01 22:40:37 -0500

    Open/closed status
    Spent 3.5 hours today on advanced JS & CSS courses and kind of burnt out for the day, but got my head around a bit of the date/time checking needed.

--- Citrouille/pumpkin.js
+++ Citrouille/pumpkin.js
@@ -1,25 +1,38 @@
-const nav = document.querySelector("nav");
-const topOfNav = nav.offsetTop;
-const flipBtn1 = document.querySelector("#flip1");
-const flipBtn2 = document.querySelector("#flip2");
-const menuCard = document.querySelector(".thecard");
+//navigation bar script 
+const nav      = document.querySelector("nav"),
+      topOfNav = nav.offsetTop;
 
 function fixNav(){
     if(window.scrollY >= topOfNav){
       document.body.classList.add("fixed-nav");
-      //set padding so it doesn't move in a jarring manner when nav is pulled from page content
       document.body.style.paddingTop = nav.offsetHeight;
     } else {
-      //so it pops back when you scroll all the way up
       document.body.style.paddingTop = 0;
       document.body.classList.remove("fixed-nav");
     };
 };
 
-function flipMenu(){
-  menuCard.classList.toggle("flip");
-};
-
 window.addEventListener("scroll", fixNav);
+
+//menu flip script
+const flipBtn1 = document.querySelector("#flip1"),
+      flipBtn2 = document.querySelector("#flip2"),
+      menuCard = document.querySelector(".thecard");
+
+function flipMenu(){menuCard.classList.toggle("flip");};
+
 flipBtn1.addEventListener("click", flipMenu);
 flipBtn2.addEventListener("click", flipMenu);
+
+//open/closed status script
+const statusMsg = document.querySelector(".openStatus");
+
+function checkOpen() {
+  const day = new Date(Date.now()).getDay();
+  const hour = new Date(Date.now()).getHours();
+  if(day >= 1 && day <=6 && hour >= 17 && hour <= 23){
+      statusMsg.textContent = `Open`;
+  } else {statusMsg.textContent = `Closed`;};
+};
+
+checkOpen();
