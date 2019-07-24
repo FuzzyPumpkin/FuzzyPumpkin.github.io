8660b1c19b9a14fae4e70059cc716cbe28c66e5b
FuzzyPumpkin <43913621+FuzzyPumpkin@users.noreply.github.com>
2019-07-24 13:25:51 -0500

    locating APIs

---
+++ ApiHappi/src/Recipe.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+
+// https://www.food2fork.com/api/get?key=cf84275fc95108a351fb7c5035b53ba7&rId=35382
+// ID 700 - 5000 safe
+// do something with date to determine number
+
+
+class Recipe extends Component {
+    constructor(props){
+        super(props);
+        this.state = {};
+    }
+
+
+    render(){
+        return (
+            <section className="section-word app">
+               
+            </section>
+        )
+    }
+}
+
+export default Recipe;
