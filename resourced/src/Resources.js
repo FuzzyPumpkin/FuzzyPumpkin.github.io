1c1ff854bd2f73b079f292fa690dfd1a560befa2
Kari <kariminger@gmail.com>
2019-08-17 21:33:35 -0500

    Created resource list format

---
+++ resourced/src/Resources.js
@@ -0,0 +1,58 @@
+const resources = [
+    {   
+        "id": 0,
+        "name": "Photopea",
+        "siteURL": "https://www.photopea.com/",
+        "imgURL": "https://fuzzypumpkin.github.io/resourced/public/images/Photopea.jpg",
+        "description": "A free image editing tool with many of the same features as Photoshop.",
+        "tags": ["Tools", "Graphics"]
+    },
+    {   
+        "id": 1,
+        "name": "Moving Head (me)",
+        "siteURL": "https://codepen.io/maaarj/full/vmYPqm",
+        "imgURL": "",
+        "description": "Animation that moves according to mouse position.",
+        "tags": ["Codepen", "Inspiration"]
+    },
+    {   
+        "id": 2,
+        "name": "Bootstrap",
+        "siteURL": "https://getbootstrap.com/",
+        "imgURL": "",
+        "description": "Ubiquitous CSS library.",
+        "tags": ["CSS", "Library"]
+    },
+    {   
+        "id": 3,
+        "name": "Codewars",
+        "siteURL": "https://www.codewars.com/",
+        "imgURL": "https://fuzzypumpkin.github.io/resourced/public/images/Codewars.jpg",
+        "description": "Ranked practice problems in a myriad of languages.",
+        "tags": ["JavaScript", "Ruby", "Practice"]
+    },
+    {   
+        "id": 4,
+        "name": "FreeCodeCamp",
+        "siteURL": "https://learn.freecodecamp.org/",
+        "imgURL": "",
+        "description": "Comprehesive online bootcamp with six different certification programs.",
+        "tags": ["CSS", "JavaScript", "HTML", "Course"]
+    },
+    {   
+        "id": 5,
+        "name": "CodeAnalogies",
+        "siteURL": "https://www.codeanalogies.com/",
+        "imgURL": "",
+        "description": "Myriad tutorials over many specific topics.",
+        "tags": ["CSS", "JavaScript", "HTML", "API", "Tutorial", "React"]
+    },
+    {   
+        "id": 6,
+        "name": "Rings Navigation Concept",
+        "siteURL": "https://codepen.io/bennettfeely/full/qRJOZJ",
+        "imgURL": "",
+        "description": "Nav similar to hamburger that expands into concentric rings.",
+        "tags": ["Codepen", "Inspiration"]
+    }
+]
