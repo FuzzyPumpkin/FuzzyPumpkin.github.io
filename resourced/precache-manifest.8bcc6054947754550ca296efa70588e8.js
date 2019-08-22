8e1c509c7f53d8ce0f904e50351726e63fcd412b
Kari <kariminger@gmail.com>
2019-08-21 20:41:39 -0500

    Deployment attempt

---
+++ resourced/precache-manifest.8bcc6054947754550ca296efa70588e8.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b33842aadb9347df69553c56c52cb985",
+    "url": "/resourced/index.html"
+  },
+  {
+    "revision": "3038fc9a1a2e07457e92",
+    "url": "/resourced/static/js/2.e0cefd3b.chunk.js"
+  },
+  {
+    "revision": "116f58d8136052630eef",
+    "url": "/resourced/static/js/main.62fc0433.chunk.js"
+  },
+  {
+    "revision": "eecc515023e1a7f57ea7",
+    "url": "/resourced/static/js/runtime~main.10914049.js"
+  }
+]);
--- resourced/src/index.js
+++ resourced/src/index.js
@@ -4,4 +4,4 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
 
-ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
+ReactDOM.render(<BrowserRouter basename={process.env.PUBLIC_URL}><App /></BrowserRouter>, document.getElementById('root'));
