1d96fb607da151c8c17751b6ec777cca7e573fae
Kari <kariminger@gmail.com>
2019-10-02 21:29:54 -0500

    Nav bar solidification effect

--- ByeByeBuggy/index.js
+++ ByeByeBuggy/index.js
@@ -1,3 +1,27 @@
+// Navigation logo display
+const secondScreen = document.querySelector(".product");
+const logo = document.querySelector(".navigation__logo");
+
+function isVisible() {
+    let displayHeight = secondScreen.getBoundingClientRect().top;
+    console.log(displayHeight);
+    if(displayHeight < 100) {
+        logo.classList.add("shown");
+    };
+};
+
+function throttle(functIn, waitTime) {
+    let time = Date.now();
+    return function() {
+      if ((time + waitTime - Date.now()) < 0) {
+        functIn();
+        time = Date.now();
+        };
+      };
+  };
+  
+document.addEventListener("scroll", throttle(isVisible, 500));
+// Product features popups
 const buttons = document.querySelectorAll(".product__icon");
 const messages = document.querySelectorAll(".product__part");
 
