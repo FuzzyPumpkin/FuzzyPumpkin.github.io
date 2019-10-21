4eb96c4a4826a708796f4fd69437dee90cc7669c
Kari <kariminger@gmail.com>
2019-10-20 22:24:24 -0500

    planning notes

--- bento/src/App.js
+++ bento/src/App.js
@@ -10,3 +10,20 @@ function App() {
 }
 
 export default App;
+
+// APP SETUP
+// Components
+// 1. box list
+// 2. box design (routing to view w/props or new view)
+//   3. choice form (1 component, 4 instances)
+//   4. save form
+// start with save to local storage
+// pre-populate one box for box list
+
+// NPM packages to use
+// 1. form validator (import {ValidatorForm, TextValidator} from "react-material-ui-form-validator")
+// 2. dragability (import {SortableContainer} from "react-sortable-hoc")
+// 3. react-router-dom
+
+// SASS is probably overkill
+// all in hooks/functional components
