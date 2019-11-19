b41bd2c4c9e0e2e43bbd8be314d9b7a562301dd9
FuzzyPumpkin <43913621+FuzzyPumpkin@users.noreply.github.com>
2019-11-19 13:06:26 -0600

    interim work/ Firebase integration

--- organizm/src/App.js
+++ organizm/src/App.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Header from "./components/layout/Header";
 import Content from "./components/layout/Content";
 import Footer from "./components/layout/Footer";
 import { ThemeProvider } from './context/theme-context';
+import {useTasks} from "./hooks/index";
 
 function App() {
+  const initialTasks = [
+    {id: 1, task: "Clean Fishtank", completed: false},
+    {id: 2, task: "Wash Car", completed: true}
+  ];
+  const [tasks, setTasks] = useState(initialTasks);
+  const tempTasks = useTasks("12345", tasks);
+  console.log(tasks);
+  console.log(tempTasks);
+
   return (
         <ThemeProvider>
           <div className="App">
--- organizm/src/hooks/index.js
+++ organizm/src/hooks/index.js
@@ -3,9 +3,16 @@ import {firebaseApp} from "../firebase.js";
 // import moment from "moment"; when we have dates added
 
 
-const firebaseDatabase = firebaseApp.database().ref().child('tasks');
 
-export function useTasks(userId){
-  console.log(firebaseDatabase);
+const firebaseTasks = firebaseApp.database().ref().child('tasks');
 
+export function useTasks(userId, tasks){
+  const userTasks = tasks;
+  //this is not working. Cleared FB database. Need to put add form in and try adding, may be how I'm referencing the collection.
+  for(let i=0; i < firebaseTasks.length; i++){
+    if (firebaseTasks[i].userId === userId){
+      userTasks.push(firebaseTasks[i]);
+    }
+  }
+  return userTasks;
 };
