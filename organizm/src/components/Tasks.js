bf6ac43d8fb93a68bafd44a481b9c401ed063e69
Kari <kariminger@gmail.com>
2019-12-03 22:48:07 -0600

    Work on basic hook task list format

--- organizm/src/components/Tasks.js
+++ organizm/src/components/Tasks.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 export default function Tasks(props) {
+    const initialTasks = [
+        {id: 1, task: "Make Task List", completed: true},
+        {id: 2, task: "???", completed: false},
+        {id: 3, task: "Profit!", completed: false}
+    ];
+    const [tasks, setTasks] = useState(initialTasks);
     return (
         <div className="tasks">
-            <h1>Hello</h1>
+            {/* task entry component */}
+            <div className="tasks__container">
+                <ul className="tasks__list">
+                {tasks.map(task => (
+                    <li className={`tasks__item ${task.completed && 'tasks__item--completed'}`} key={task.id}>
+                        {task.task}
+                    </li>
+                ))}
+                </ul>
+            </div>
+            {/* search/sort component */}
         </div>
     )
 }
