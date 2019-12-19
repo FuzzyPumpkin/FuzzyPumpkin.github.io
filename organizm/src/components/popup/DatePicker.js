aa2b216b120e5d96347c38f89f21abadc3eb9393
FuzzyPumpkin <43913621+FuzzyPumpkin@users.noreply.github.com>
2019-12-19 14:35:58 -0600

    days in correct spot on calendar

--- organizm/src/components/popup/DatePicker.js
+++ organizm/src/components/popup/DatePicker.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-
+import uuid from "uuid";
 
 
 export default function DatePicker(props) {
@@ -9,7 +9,18 @@ export default function DatePicker(props) {
     let currYear = currDate.getFullYear(); 
     const [chosenDate, setChosenDate] = useState({month: currMonth, day: currDay, year: currYear});
 
-    const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    const monthNames = [{month: "January", days: 31},
+                        {month: "February", days: 28},
+                        {month: "March", days: 31},
+                        {month: "April", days: 31},
+                        {month: "May", days: 31},
+                        {month: "June", days: 30},
+                        {month: "July", days: 31},
+                        {month: "August", days: 31},
+                        {month: "September", days: 30},
+                        {month: "October", days: 31},
+                        {month: "November", days: 30},
+                        {month: "December", days: 31}];
     const dateBack = () => {
         const lastMonth = chosenDate.month === 1 ? 12 : chosenDate.month - 1;
         const day = chosenDate.day;
@@ -23,13 +34,32 @@ export default function DatePicker(props) {
         const year = nextMonth === 1 ? chosenDate.year + 1 : chosenDate.year;
         setChosenDate({month: nextMonth, day: day, year: year});
     }
+
+    const calendarDates = () => {
+        let dates = [];
+        for( let i = 1; i <= monthNames[chosenDate.month - 1].days; i++){
+        dates.push(i);
+        }
+        return dates;
+    }
+    const blankDates = () => {
+        let blanks = [];
+        let dateName = `${chosenDate.month} 1, ${chosenDate.year}`;
+        let day = new Date(dateName).getDay();
+        if(day !== 0){
+            for( let i = 0; i < day; i++){
+                blanks.push(i);
+            }
+        }
+        return blanks;
+    }
     
     return (
         <div className="datepicker">
             <div className="datepicker__container">
                 <p className="datepicker__chosen">
                     <span className="datepicker__chosen-dateDisplay"> {chosenDate.month}/{chosenDate.day}/{chosenDate.year}</span>
-                    <button className="datepicker__button datepicker__button--save" aria-label="Save Date" onClick={() => props.updTaskDate(chosenDate)}>
+                    <button type="button" className="datepicker__button datepicker__button--save" aria-label="Save Date" onClick={() => props.updTaskDate(chosenDate)}>
                         <svg className="tasks__button-icon">
                             <use xlinkHref="./images/symbol-defs.svg#icon-save"></use>
                         </svg>
@@ -37,51 +67,22 @@ export default function DatePicker(props) {
                 </p>
                 <div className="datepicker__calendar">
                     <div className="datepicker__month">
-                        <button className="datepicker__button" aria-label="Go Back" onClick={dateBack}>
+                        <button type="button" className="datepicker__button" aria-label="Go Back" onClick={dateBack}>
                             &lt;
                         </button>
-                        <span className="datepicker__monthText">{monthNames[chosenDate.month - 1]}</span>
-                        <button className="datepicker__button" aria-label="Go Forward" onClick={dateForward}>
+                        <span className="datepicker__monthText">{monthNames[chosenDate.month - 1].month}</span>
+                        <button type="button" className="datepicker__button" aria-label="Go Forward" onClick={dateForward}>
                             &gt;
                         </button>
                     </div>
                     <div className="datepicker__days">
-                        <div className="datepicker__day">1</div>
-                        <div className="datepicker__day">2</div>
-                        <div className="datepicker__day">3</div>
-                        <div className="datepicker__day">4</div>
-                        <div className="datepicker__day">5</div>
-                        <div className="datepicker__day">6</div>
-                        <div className="datepicker__day">7</div>
-                        <div className="datepicker__day">1</div>
-                        <div className="datepicker__day">2</div>
-                        <div className="datepicker__day">3</div>
-                        <div className="datepicker__day">4</div>
-                        <div className="datepicker__day">5</div>
-                        <div className="datepicker__day">6</div>
-                        <div className="datepicker__day">7</div>
-                        <div className="datepicker__day">1</div>
-                        <div className="datepicker__day">2</div>
-                        <div className="datepicker__day">3</div>
-                        <div className="datepicker__day">4</div>
-                        <div className="datepicker__day">5</div>
-                        <div className="datepicker__day">6</div>
-                        <div className="datepicker__day">7</div>
-                        <div className="datepicker__day">1</div>
-                        <div className="datepicker__day">2</div>
-                        <div className="datepicker__day">3</div>
-                        <div className="datepicker__day">4</div>
-                        <div className="datepicker__day">5</div>
-                        <div className="datepicker__day">6</div>
-                        <div className="datepicker__day">7</div>
-                        <div className="datepicker__day">1</div>
-                        <div className="datepicker__day">2</div>
-                        <div className="datepicker__day">3</div>
-                        <div className="datepicker__day">4</div>
-                        <div className="datepicker__day">5</div>
-                        <div className="datepicker__day">6</div>
-                        <div className="datepicker__day">7</div>
-                   
+                        {blankDates().map(date => (
+                            <div className="datepicker__day" key={uuid()}></div>
+                        ))}
+                        {calendarDates().map(date => (
+                                <div className="datepicker__day" key={date}>{date}</div>
+                            ))
+                        }
                     </div>
                 </div>
             </div>
