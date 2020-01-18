5126d6bb19660b015241dc04d5255af01359c728
Kari <kariminger@gmail.com>
2020-01-17 21:18:20 -0600

    Add functionality to play/rew/ff buttons

--- fiftytwo/videoPlayer/scripts.js
+++ fiftytwo/videoPlayer/scripts.js
@@ -1,38 +1,42 @@
-const   player      = document.querySelector(".player"),
-        video       = player.querySelector(".viewer"),
-        progress    = player.querySelector(".progress"),
-        progressBar = player.querySelector(".progress__filled"),
-        toggle      = player.querySelector(".toggle"),
-        skipButtons = player.querySelectorAll("[data-skip]"),
-        ranges      = player.querySelectorAll(".player__slider");
+const video = document.querySelector(".video__screen");
 
-toggle.addEventListener("click", togglePlay);
+const playButton = document.querySelector("#playButton");
+const playIcon = document.querySelector("#playIcon");
+const pauseIcon = document.querySelector("#pauseIcon");
+const ffButton = document.querySelector(".buttons__button--ff");
+const rewButton = document.querySelector(".buttons__button--rew");
+const volumeBar = document.querySelector(".volume__bar");
+const progressBar = document.querySelector(".progress__bar");
+// const progress = player.querySelector(".progress");
+// const progressBar = player.querySelector(".progress__filled");
+// const ranges = player.querySelectorAll(".player__slider");
+
+playButton.addEventListener("click", togglePlay);
 video.addEventListener("click",togglePlay);
-video.addEventListener("play", updButton);
-video.addEventListener("pause", updButton);
-skipButtons.forEach(button => button.addEventListener("click", skip));
-ranges.forEach(range => range.addEventListener("change", rangeUpdate));
-video.addEventListener("timeupdate", updProgress);
-progress.addEventListener("click", scrub);
+ffButton.addEventListener("click", skip);
+rewButton.addEventListener("click", skip);
+// ranges.forEach(range => range.addEventListener("change", rangeUpdate));
+// video.addEventListener("timeupdate", updProgress);
+// progress.addEventListener("click", scrub);
 
 function togglePlay(){
     video.paused ? video.play() : video.pause();
+    playIcon.classList.toggle("buttons__svg--hidden");
+    pauseIcon.classList.toggle("buttons__svg--hidden");
+    playButton.classList.toggle("buttons__playButton--pause");
 };
-function updButton(){
-    const playIcon = this.paused ? '►' : '❚ ❚';
-    toggle.textContent = playIcon;
-};
+
 function skip(){
     video.currentTime += parseFloat(this.dataset.skip);
 };
-function rangeUpdate(){
-    video[this.name] = this.value;
-};
-function updProgress(){
-    const percent = (video.currentTime / video.duration) * 100;
-    progressBar.style.flexBasis = `${percent}%`;
-};
-function scrub(e){
-    const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
-    video.currentTime = scrubTime;
-};
+// function rangeUpdate(){
+//     video[this.name] = this.value;
+// };
+// function updProgress(){
+//     const percent = (video.currentTime / video.duration) * 100;
+//     progressBar.style.flexBasis = `${percent}%`;
+// };
+// function scrub(e){
+//     const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+//     video.currentTime = scrubTime;
+// };
