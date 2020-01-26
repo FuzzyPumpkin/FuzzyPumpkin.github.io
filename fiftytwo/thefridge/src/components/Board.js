5eecefa649a5cc0b0cde88643feb3f2b683d4b3b
Kari <kariminger@gmail.com>
2020-01-26 16:17:01 -0600

    Refine fridge background svg files

--- fiftytwo/thefridge/src/components/Board.js
+++ fiftytwo/thefridge/src/components/Board.js
@@ -1,36 +1,31 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
 import WordMagnet from "./WordMagnet";
 import EmojiMagnet from "./EmojiMagnet";
-import nouns from "../dictionaries/nouns.js"
+// import nouns from "../dictionaries/nouns.js"
 
 export default function Board() {  
-    const [words, setWords] = useState([]);
-    const [emojis, setEmojis] = useState([]);
+    const [words, setWords] = useState(["kitten", "party", "run"]);
+    const [emojis, setEmojis] = useState(["angry", "poo"]);
 
 
-    const makeWordList = () => {
-        let newWords = [];
-        for(let i = 0; i <= 5; i++){
-            // let randomNoun = nouns[Math.floor(Math.random * nouns.length)];
-            // console.log(randomNoun);
-            let randomNoun = nouns[i];
-            console.log(randomNoun);
-            newWords.push(randomNoun);
-        }
-        setWords(newWords);
-    }
-
-    // useEffect(() => {
-    //     makeWordList();
-    // }, [words]);
-
+    // const makeWordList = () => {
+    //     let newWords = [];
+    //     for(let i = 0; i <= 5; i++){
+    //         let randomNoun = nouns[Math.floor(Math.random * nouns.length)];
+    //         console.log(randomNoun);
+    //         let randomNoun = nouns[i];
+    //         console.log(randomNoun);
+    //         newWords.push(randomNoun);
+    //     }
+    //     setWords(newWords);
+    // }
     return (
         <div className="Board">
-            <p> The play board will appear here </p>
-            <WordMagnet word="apple" />
-            <EmojiMagnet emoji="angry" />
             {words.map(word => (
-                <WordMagnet word={word} />))
+                <WordMagnet word={word} key={word}/>))
+            }
+            {emojis.map(emoji => (
+                <EmojiMagnet emoji={emoji} key={emoji}/>))
             }
         </div>
     )
