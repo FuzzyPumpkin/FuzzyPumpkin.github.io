c3e835a87196beb210b27314d8f917c39aee5fde
FuzzyPumpkin <43913621+FuzzyPumpkin@users.noreply.github.com>
2020-01-28 12:56:03 -0600

    Update the background for app

--- fiftytwo/thefridge/src/App.js
+++ fiftytwo/thefridge/src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <Board />
       <div className="controls">
+        <div className="controls__row">
         <h1>The Fridge</h1>
         <button className="controls__button" type="button">
           <span className="controls__buttonText">new words</span>
@@ -13,9 +14,10 @@ function App() {
             <use xlinkHref="./images/symbol-defs.svg#icon-splat"></use>
           </svg>
         </button>
-      </div>
-      <p className="attribution">Emoji icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com.</a>
+        </div>
+      <p className="controls__attribution">Emoji icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com.</a>
       </p>
+      </div>
     </div>
   );
 }
