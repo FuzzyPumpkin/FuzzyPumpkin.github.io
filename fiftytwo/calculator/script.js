912fde288f7a6f9d07a4e69bec01c637585aebeb
Kari <kariminger@gmail.com>
2020-03-09 17:56:46 -0500

    Complete functionality

--- fiftytwo/calculator/script.js
+++ fiftytwo/calculator/script.js
@@ -9,54 +9,77 @@ const displaySpace = document.querySelector(".calculator__display");
 let currDisplay = "";
 let savedNum = "";
 let lastOper = "";
+let clearDisplay = false;
 
 addListeners();
 
 function addListeners(){
 	for(let i = 0; i < numButtons.length; i++){
 		numButtons[i].addEventListener("click", function(){
+			if(clearDisplay){
+				currDisplay = "";
+				displaySpace.innerHTML = currDisplay;
+			};
 			let oldDisplay = displaySpace.innerHTML;
 			currDisplay = oldDisplay + this.id;
 			displaySpace.innerHTML = currDisplay;
-			//problem. If display is showing result of a calculation (but no equal), adds number to that result 
-			//instead of clearing.
 		});
 	};
 	for(let i = 0; i < operButtons.length; i++){
 		operButtons[i].addEventListener("click", function(){
-			if(lastOper.length <= 0){
+			if(lastOper.length <= 0 || lastOper === "equal"){
 				lastOper = this.id;
 				savedNum = currDisplay;
 				displaySpace.innerHTML = "";
 				console.log("blank");
 			} else {
 				runCalculation(this.id);
-			}
+			};
 		})
 	}
+	for(let i = 0; i < tipButtons.length; i++){
+		tipButtons[i].addEventListener("click", function(){
+			let calcNum;
+			if(this.id === "fifteen"){
+				calcNum = parseFloat(currDisplay) * .15;
+			} else {
+				calcNum = parseFloat(currDisplay) * .2;
+			};
+			currDisplay = calcNum;
+			displaySpace.innerHTML = currDisplay;
+			clearDisplay = true;
+		});
+	};
 };
 function runCalculation(currOper){
 	let calcNum;
 	if(lastOper === "plus"){
-		calcNum = parseInt(savedNum) + parseInt(currDisplay);
+		calcNum = parseFloat(savedNum) + parseFloat(currDisplay);
 	} else if(lastOper === "minus"){
-		calcNum = parseInt(savedNum) - parseInt(currDisplay);
+		calcNum = parseFloat(savedNum) - parseFloat(currDisplay);
 	} else if(lastOper === "multiply"){
-		calcNum = parseInt(savedNum) * parseInt(currDisplay);
+		calcNum = parseFloat(savedNum) * parseFloat(currDisplay);
 	} else{
-		calcNum = parseInt(savedNum) / parseInt(currDisplay);
-	}
+		calcNum = parseFloat(savedNum) / parseFloat(currDisplay);
+	};
 	currDisplay = calcNum;
 	displaySpace.innerHTML = currDisplay;
 	lastOper = currOper;
+	clearDisplay = true;
 }
 intButton.addEventListener("click", function(){
-	oldDisplay = currDisplay + ".";
-	displaySpace.innerHTML = currDisplay;
-})
+	if(currDisplay[currDisplay.length - 1] !== "."){
+		newDisplay = currDisplay + ".";
+		displaySpace.innerHTML = newDisplay;
+		currDisplay = newDisplay;
+	};
+});
 clearButton.addEventListener("click", function(){
 	currDisplay = "";
 	savedNum = "";
 	lastOper = "";
 	displaySpace.innerHTML = "";
-})
+});
+eqButton.addEventListener("click", function(){
+	lastOper !== "equal" ? runCalculation(this.id) : null;
+});
