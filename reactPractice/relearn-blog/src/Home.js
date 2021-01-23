57e1d836da4f999c29269a60a437bf69d806753a
Kari <kariminger@gmail.com>
2021-01-23 14:23:31 -0600

    follow tutorial
    Following along with NetNinja tutorial (all remedial), with a few of my own changes.

---
+++ reactPractice/relearn-blog/src/Home.js
@@ -0,0 +1,8 @@
+const Home = () => {
+    return (
+        <nav className="home">
+            <h2>Homepage</h2>
+        </nav>
+    )
+}
+export default Home;
---
+++ reactPractice/relearn-blog/src/Navbar.js
@@ -0,0 +1,12 @@
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <h1>The BLOG</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/create">New Entry</a>
+            </div>
+        </nav>
+    )
+}
+export default Navbar;
