232796fc00ba047ba07f103e3b6e95fd2082441b
Kari <kariminger@gmail.com>
2021-01-24 18:39:07 -0600

    refactor state setup
    refactored main function (which still needs a good deal of work) to store sim attributes in one object in state instead of multiple strings/arrays/etc.

--- fiftytwo/makeasim/src/Roller.js
+++ fiftytwo/makeasim/src/Roller.js
@@ -7,37 +7,41 @@ import childTraits from "./dictionaries/childTraits.js";
 import ages from "./dictionaries/ages.js";
 
 function Roller() {
-  const [career, setCareer] = useState([""]);
-  const [traitList, setTraitList] = useState([""]);
-  const [aspiration, setAspiration] = useState("");
-  const [simAge, setSimAge] = useState("");
-  const [simGender, setSimGender] = useState("");
-  const [orientation, setOrientation] = useState("");
-  const [isSpawn, setIsSpawn] = useState(false);
+
+  const [simList, setSimList] = useState([{
+    age: "",
+    aspiration: "",
+    career: [""],
+    traits: [""],
+    gender: "",
+    sexualOrientation: "",
+    isSpawn: false, 
+    key: 1
+  }]);
 
   const rollOptions = () => {
+    console.log("clicked");
+    let simSetup = {};
+    simSetup.key = Math.floor(Math.random() * 1000);
     let randAge = Math.floor(Math.random() * ages.length);
-    let rolledSimAge = ages[randAge];
-    setSimAge(rolledSimAge);
-    console.log(rolledSimAge);
-    //TRAITS - NEED CHECK FOR EP
-    if(rolledSimAge === "toddler"){
+    simSetup.age = ages[randAge];
+
+    if(simSetup.age === "toddler"){
       let newTrait = [];
       let randTraitNum = Math.floor(Math.random() * toddlerTraits.length);
       newTrait.push(toddlerTraits[randTraitNum]);
-      setTraitList(newTrait);
-      setIsSpawn(true);
-    } else if(rolledSimAge === "child"){
+      simSetup.traits = newTrait;
+      simSetup.isSpawn = true;
+    } else if(simSetup.age === "child"){
       let newTrait = [];
       let randTraitNum = Math.floor(Math.random() * childTraits.length);
       newTrait.push(childTraits[randTraitNum].trait);
-      setTraitList(newTrait);
-      setIsSpawn(true);
+      simSetup.traits = newTrait;
+      simSetup.isSpawn = true;
     } else {
-      console.log("in adult roll");
-      let traitNum = simAge === "teen" ? 2 : 3;
       let newTraits = [];
-      for(let i = 0; i < traitNum; i++){
+      for(let i = 0; i < 3; i++){
+        console.log("firing trait roll");
         for(let j = 0; j < i; j++){
           let randTraitNum = Math.floor(Math.random() * traits.length);
           if(newTraits.includes(traits[randTraitNum].trait) || traits[randTraitNum].conflicts.includes(newTraits[j])){
@@ -47,50 +51,54 @@ function Roller() {
           }
         }
       }
-      setTraitList(newTraits);
-      setIsSpawn(false);
-      //CAREER - NEED TO CHECK FOR EP
+      if(simSetup.age === "teen"){newTraits.pop()};
+      simSetup.traits = newTraits;
+      simSetup.isSpawn = false;
       let randCareerNum = Math.floor(Math.random() * careers.length);
-      setCareer(careers[randCareerNum].career);
-      //ASPIRATION - NEED TO CHECK FOR EP
+      simSetup.career = careers[randCareerNum].career;
       let randAspirationNum = Math.floor(Math.random() * aspirations.length);
-      setAspiration(aspirations[randAspirationNum].aspiration);
+      simSetup.aspiration = aspirations[randAspirationNum].aspiration;
       let randOrientationNum = Math.floor(Math.random() * 100);
       if(randOrientationNum <= 75){
-        setOrientation("straight");
+        simSetup.sexualOrientation = "straight";
       } else if(randOrientationNum <= 90){
-        setOrientation("gay");
-      } else{setOrientation("bisexual");}
+        simSetup.sexualOrientation = "gay";
+      } else{simSetup.sexualOrientation = "bisexual";}
     };
     let randGender = Math.floor(Math.random() * 2);
     if(randGender === 0){
-      setSimGender("female");
-    } else{setSimGender("male");};
+      simSetup.gender = "female";
+    } else{simSetup.gender = "male";};
+    console.log(simSetup);
+    setSimList([simSetup]);
    };
-
+  
   return (
     <div className="Roller">
       <h1>Sim Roller</h1>
       <button type="button" onClick={rollOptions}>Roll</button>
       <h3>Result:</h3>
-      <p>Age: {simAge}</p>
-      <p>Gender: {simGender}</p>
-      <p>Traits:</p>
+      {simList.map(sim => (
+        <div key={sim.key}>
+        <p>Age: {sim.age}</p>
+        <p>Gender: {sim.gender}</p>
+        <p>Traits:</p>
         <ul>
-        {traitList.map(trait => (
-          <li key={trait}>
-            {trait}
-          </li>))}
-        </ul>
-      { isSpawn ? null : (
-        <div>
-          <p>Aspiration: {aspiration}</p>
-          <p>Career: {career}</p>
-          <p>Sexual Orientation: {orientation}</p>
-        </div>
+          {sim.traits.map(trait => (
+            <li key={trait}>
+              {trait}
+            </li>))}
+          </ul>
+       { sim.isSpawn ? null : (
+          <div>
+            <p>Aspiration: {sim.aspiration}</p>
+            <p>Career: {sim.career}</p>
+            <p>Sexual Orientation: {sim.sexualOrientation}</p>
+          </div>
         )
       }
-    
+        </div>
+      ))}
     </div>
   );
 }
