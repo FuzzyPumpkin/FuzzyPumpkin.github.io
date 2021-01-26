a0df2b048d3c117d636a93dd3d447935c341d042
Kari <kariminger@gmail.com>
2021-01-26 13:22:46 -0600

    refactor main roller function
    Split anything redundant into separate functions and abstracted.

--- fiftytwo/makeasim/src/Roller.js
+++ fiftytwo/makeasim/src/Roller.js
@@ -23,25 +23,17 @@ function Roller() {
     console.log("clicked");
     let simSetup = {};
     simSetup.key = Math.floor(Math.random() * 1000);
-    let randAge = Math.floor(Math.random() * ages.length);
-    simSetup.age = ages[randAge];
+    simSetup.age = aspectFinder(ages);
 
     if(simSetup.age === "toddler"){
-      let newTrait = [];
-      let randTraitNum = Math.floor(Math.random() * toddlerTraits.length);
-      newTrait.push(toddlerTraits[randTraitNum]);
-      simSetup.traits = newTrait;
+      simSetup.traits = spawnTraitFinder(toddlerTraits);
       simSetup.isSpawn = true;
     } else if(simSetup.age === "child"){
-      let newTrait = [];
-      let randTraitNum = Math.floor(Math.random() * childTraits.length);
-      newTrait.push(childTraits[randTraitNum].trait);
-      simSetup.traits = newTrait;
+      simSetup.traits = spawnTraitFinder(childTraits);
       simSetup.isSpawn = true;
     } else {
       let newTraits = [];
       for(let i = 0; i < 3; i++){
-        console.log("firing trait roll");
         for(let j = 0; j < i; j++){
           let randTraitNum = Math.floor(Math.random() * traits.length);
           if(newTraits.includes(traits[randTraitNum].trait) || traits[randTraitNum].conflicts.includes(newTraits[j])){
@@ -54,10 +46,8 @@ function Roller() {
       if(simSetup.age === "teen"){newTraits.pop()};
       simSetup.traits = newTraits;
       simSetup.isSpawn = false;
-      let randCareerNum = Math.floor(Math.random() * careers.length);
-      simSetup.career = careers[randCareerNum].career;
-      let randAspirationNum = Math.floor(Math.random() * aspirations.length);
-      simSetup.aspiration = aspirations[randAspirationNum].aspiration;
+      simSetup.career = aspectFinder(careers).career;
+      simSetup.aspiration = aspectFinder(aspirations).aspiration;
       let randOrientationNum = Math.floor(Math.random() * 100);
       if(randOrientationNum <= 75){
         simSetup.sexualOrientation = "straight";
@@ -66,12 +56,24 @@ function Roller() {
       } else{simSetup.sexualOrientation = "bisexual";}
     };
     let randGender = Math.floor(Math.random() * 2);
-    if(randGender === 0){
-      simSetup.gender = "female";
-    } else{simSetup.gender = "male";};
+    randGender === 0 ? simSetup.gender = "female" : simSetup.gender = "male"; 
+    //REMOVE THIS BEFORE DONE!!
     console.log(simSetup);
     setSimList([simSetup]);
    };
+
+   const spawnTraitFinder = (ageGroupTraits) => {
+    let newTrait = [];
+    let randTraitNum = Math.floor(Math.random() * ageGroupTraits.length);
+    newTrait.push(ageGroupTraits[randTraitNum].trait);
+    return newTrait;
+   };
+
+   const aspectFinder = (aspect, aspectKey) => {
+    let randNum = Math.floor(Math.random() * aspect.length);
+    return aspect[randNum];
+   };
+
   
   return (
     <div className="Roller">
--- fiftytwo/makeasim/src/dictionaries/toddlerTraits.js
+++ fiftytwo/makeasim/src/dictionaries/toddlerTraits.js
@@ -1,4 +1,12 @@
-const toddlerTraits = 
-["Angelic", "Charmer", "Clingy", "Fussy", "Independent", "Inquisitive", "Silly", "Wild"];
+const toddlerTraits = [
+    {trait: "angelic", conflicts: [], expansion: ""},
+    {trait: "charmer", conflicts: [], expansion: ""},
+    {trait: "clingy", conflicts: [], expansion: ""},
+    {trait: "fussy", conflicts: [], expansion: ""},
+    {trait: "independent", conflicts: [], expansion: ""},
+    {trait: "inquisitive", conflicts: [], expansion: ""},
+    {trait: "silly", conflicts: [], expansion: ""},
+    {trait: "wild", conflicts: [], expansion: ""},
+];
 
 export default toddlerTraits;
