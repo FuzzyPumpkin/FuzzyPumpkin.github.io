a8dab897d37958e5734455ddd820f7b74ea69d7d
Kari <kariminger@gmail.com>
2021-09-30 16:12:01 -0500

    update favicon, ready code for styling

--- fiftytwo/scrappy/scrappy.js
+++ fiftytwo/scrappy/scrappy.js
@@ -1,9 +1,9 @@
 const displayList = document.querySelector(".displayList");
-const addButton = document.querySelector(".add");
+const addButton = document.querySelector(".form__addBtn");
 const chooseButton = document.querySelector(".choose");
-const newColor = document.querySelector(".newColor");
+const newColor = document.querySelector(".form__newColor");
 const savedColorList = localStorage.savedColorList;
-const colorPicker = document.querySelector(".colorPicker");
+const colorPicker = document.querySelector(".form__colorPicker");
 let colorList = [];
 const choices = document.querySelector(".choices");
 
@@ -17,7 +17,7 @@ const delButtons = document.querySelectorAll(".delete");
 
 addButton.addEventListener("click", function(){
     let newElement = document.createElement("li");
-    let buttonTag = '<button class="delete" id=' + colorPicker.value + '>del</button>';
+    let buttonTag = '<button class="delete" id=' + colorPicker.value + '><svg class="delete__image"><use xlink:href="images/symbol-defs.svg#icon-delete"></use></svg></button>';
     newElement.innerHTML = newColor.value + '<div class="displayList__daub" style="background-color:' + colorPicker.value + '"></div>' + buttonTag; 
     newElement.accessKey = newColor.value;
     newElement.childNodes[1].addEventListener("click", delItem);
@@ -39,7 +39,7 @@ function delItem(){
 chooseButton.addEventListener("click", function(){
   let firstColor = colorList[Math.floor(Math.random() * colorList.length)];
   let secondColor = colorList[Math.floor(Math.random() * colorList.length)];
-  choices.innerHTML = '<p><span style="color:' + firstColor.hex +'">' + firstColor.color + '</span> <span style="color:' + secondColor.hex +'">' + secondColor.color + '</span></p>';
+  choices.innerHTML = '<div class="choices__color" style="background-color:' + firstColor.hex + '">'+ firstColor.color +'</div><div class="choices__color" style="background-color:' + secondColor.hex + '">'+ secondColor.color +'</div>';
 });
 
 
